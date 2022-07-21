1ab0f1999c66c5c332f5a158b749d758a4b9d1b3
gustavo auad piccoli <gustavo.a.piccoli@gmail.com>
2022-07-21 10:40:51 -0300

    agente bugado, mas é um começo

--- advsearch/perry_o_ornitorrinco/agent.py
+++ advsearch/perry_o_ornitorrinco/agent.py
@@ -1,11 +1,11 @@
 import random
 import sys
+import os
+caminho = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\othello"
+sys.path.append(caminho)
+import board
 
-# Voce pode criar funcoes auxiliares neste arquivo
-# e tambem modulos auxiliares neste pacote.
-#
-# Nao esqueca de renomear 'your_agent' com o nome
-# do seu agente.
+
 
 
 def make_move(the_board, color):
@@ -15,10 +15,66 @@ def make_move(the_board, color):
     :param color: a character indicating the color to make the move ('B' or 'W')
     :return: (int, int) tuple with x, y indexes of the move (remember: 0 is the first row/column)
     """
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada com as pretas.
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    #verifica se o jogo acabou
+    the_board.print_board()
+    if the_board.is_terminal_state():
+        return (-1, -1)
+    #faz a jogada
+    else:
+        (tabuleiro, cor, m, pos_x, pos_y) = max_play(the_board, color, 0, -7, 7)
+        return (pos_x, pos_y)
+
+
+
+def max_play(the_board, color, m, alfa, beta):
+    maximizar = -7
+    pos_x = None
+    pos_y = None
+    if the_board.is_terminal_state():
+        return (the_board, color, m, -1, -1)
+    for i in range (8):
+        for j in range (8):
+            if the_board.is_legal((i, j), color):  
+                the_board.tiles[i][j] = color
+                (tabuleiro, cor, m, min_i, min_j) = min_play(the_board, color, m, alfa, beta)
+                if maximizar < m:
+                    maximizar = m
+                    pos_x = i
+                    pos_y = j
+                    the_board.tiles[i][j] = '.'
+                if maximizar >= beta:
+                    return (tabuleiro, cor, maximizar, pos_x, pos_y)
+                if maximizar > alfa:
+                    alfa = maximizar
+    return (tabuleiro, cor, maximizar, pos_x, pos_y)
+
+
+
+def min_play(the_board, color, m, alfa, beta):
+    minimizar = 7
+    pos_x = None
+    pos_y = None
+    if the_board.is_terminal_state():
+        return (the_board, color, m, -1, -1)
+    for i in range (8):
+        for j in range (8):
+            if the_board.is_legal((i, j), color):  
+                if color == 'B':
+                    color_i = 'W'
+                else:
+                    color_i = 'B'
+                the_board.tiles[i][j] = color_i
+                (tabuleiro, cor, m, max_i, max_j) = max_play(the_board, color, m, alfa, beta)
+                if minimizar > m:
+                    minimizar = m
+                    pos_x = i
+                    pos_y = j
+                    the_board.tiles[i][j] = '.'
+                if minimizar <= alfa:
+                    return (tabuleiro, cor, minimizar, pos_x, pos_y)
+                if minimizar < beta:
+                    beta = minimizar
+    return (tabuleiro, cor, minimizar, pos_x, pos_y)
 
 '''
 Minhas Notas:
@@ -27,3 +83,5 @@ Um movimento é uma tupla com dois inteiros com valores entre 0 e 7
 Caso não haja movimento valido, retorna a tupla -1, -1
 O primeiro inteiro é a coluna e o segundo a linha
 '''
+o = board.Board()
+print(make_move(o, 'W'))
