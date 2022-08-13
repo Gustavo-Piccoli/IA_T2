91d0521462ad4c646fd3c3c5c8d13718e05e847a
nikolas <nikolasps7@gmail.com>
2022-08-12 21:43:07 -0300

    final commit

--- advsearch/perry_o_ornitorrinco/agent.py
+++ advsearch/perry_o_ornitorrinco/agent.py
@@ -19,7 +19,7 @@ from advsearch.perry_o_ornitorrinco.node import Node
 from advsearch.perry_o_ornitorrinco.my_heuristic import My_Heuristic
 
 
-MAX_DEPTH = 5
+MAX_DEPTH = 4
 
 class MinMax:
 
@@ -60,7 +60,7 @@ class MinMax:
                 node.value = float('inf')
                 return node.value
         if node.depth >= self.max_depth:
-            node.value = max(node.value,self.heuristic.get_state_value(node))
+            node.value = max(node.value,self.quiesce(node.alpha,node.beta,node,0))
             return node.value
         move = None
         successors = []
@@ -73,6 +73,7 @@ class MinMax:
             node.alpha  = max(node.alpha,node.value)
         node.successors = successors
         return node.value
+
     def __min_play(self,node:Node):
         """
         v = +∞ 
@@ -88,7 +89,7 @@ class MinMax:
                 node.value = float('-inf')
                 return node.value
         if node.depth >= self.max_depth:
-            node.value = min(node.value,self.heuristic.get_state_value(node))
+            node.value = min(node.value,self.quiesce(node.alpha,node.beta,node,0))
             return node.value
         move = None
         successors = []
@@ -101,7 +102,42 @@ class MinMax:
             node.beta  = min(node.beta,node.value)
         node.successors = successors
         return node.value
+    def quiesce(self,alpha, beta, node:Node, depth):
 
+        """
+         most obvious ones: forced moves (i.e. there is only one move), corner moves
+        """
+        stand_pat = self.heuristic.get_state_value(node)
+        if depth == 2:
+            return stand_pat
+        if stand_pat >= beta:
+            return beta
+        if alpha < stand_pat:
+            alpha = stand_pat
+        
+        legal_moves = node.board.legal_moves(node.color)
+        for move in legal_moves:
+            if len(legal_moves) <= 1 or self.__corner_move(move):
+                succ = self.__create_successor(node,move)
+                score = -self.quiesce( -beta, -alpha, succ, depth+1)
+                if score >= beta:
+                    return beta
+                if score > alpha:
+                    alpha = score
+        
+        return alpha
+    def __corner_move(self,move):
+        move = (move[1],move[0]) # movimento é inverso por algum motivo, bizarro
+
+        if move == (0,1) or move == (1,0) or move == (1,1) or move == (0,0):
+            return True
+        if move == (0,6) or move == (1,7) or move == (1,6) or move == (0,7):
+            return True
+        if move == (7,1) or move == (6,0) or move == (6,1) or move == (7,0):
+            return True
+        if move == (7,6) or move == (6,7) or move == (6,6) or move == (7,7): 
+            return True
+        return False 
     
     def __create_successor(self,node: Node,move):
         if node.color == node.board.BLACK:
@@ -112,40 +148,7 @@ class MinMax:
         copy_board.process_move(move, node.color)
         return Node(copy_board,opponent_color,node.alpha,node.beta,node.depth + 1,node,move)
 
-    def quiesce(self,alpha, beta, node:Node):
-
-        """
-         most obvious ones: forced moves (i.e. there is only one move), corner moves and moves to a square adjacent to a corne
-        """
-        stand_pat = self.heuristic.get_state_value(node)
-        if stand_pat >= beta:
-            return beta
-        if alpha < stand_pat:
-            alpha = stand_pat
-
-        legal_moves = node.board.legal_moves(node.color)
-        for move in legal_moves:
-            if len(legal_moves) <= 1 or self.__corner_move(move):
-                succ = self.__create_successor(node,move)
-                score = -self.quiesce( -beta, -alpha, succ)
-                if score >= beta:
-                    return beta
-                if score > alpha:
-                    alpha = score
-        
-        return alpha
-    def __corner_move(self,move):
-        move = (move[1],move[0]) # movimento é inverso por algum motivo, bizarro
-
-        if move == (0,1) or move == (1,0) or move == (1,1):
-            return True
-        if move == (0,6) or move == (1,7) or move == (1,6):
-            return True
-        if move == (7,1) or move == (6,0) or move == (6,1):
-            return True
-        if move == (7,6) or move == (6,7) or move == (6,6): 
-            return True
-        return False 
+    
         
 def make_move(the_board:Board, color):
     """
--- advsearch/perry_o_ornitorrinco/my_heuristic.py
+++ advsearch/perry_o_ornitorrinco/my_heuristic.py
@@ -21,7 +21,7 @@ class My_Heuristic(Heuristic):
                     count+=1
         return count
                   
-    def get_num_corners_at_risk(self,my_board:Board, color):
+    def get_corners_risk(self,my_board:Board, color):
         """
         get number of pre-corners tiles that the player possess that gives the risk of giving up a corner
         pre-corner - tile that can be eaten to get the corner
@@ -48,7 +48,7 @@ class My_Heuristic(Heuristic):
             count+=1
         return count
 
-    def x_square_occupied(self,my_board:Board,color):
+    def x_tile_occupied(self,my_board:Board,color):
         count=0
         if my_board.tiles[1][1] == color and my_board.tiles[0][0]  == '.':
             count+=1
@@ -73,7 +73,39 @@ class My_Heuristic(Heuristic):
                     count+=1
         return count
 
+    def is_outer_tiles_sparse(self,my_board:Board,sparse_percent):
+        empty_squares = 0
+        num_of_outer_tiles = 28
+        for j in range(0,8):
+            if my_board.tiles[0][j] == '.':
+                empty_squares+= 1
+        for j in range(0,8):
+            if my_board.tiles[7][j] == '.':
+                empty_squares+= 1
+        for i in range(1,7):
+            if my_board.tiles[i][0] == '.':
+                empty_squares+= 1
+        for i in range(1,7):
+            if my_board.tiles[i][7] == '.':
+                empty_squares+= 1
+        if empty_squares/num_of_outer_tiles >= sparse_percent:
+            return True
+        else:
+            return False
+
     def long_frontier_sequence_penalty(self,my_board:Board,color):
+
+        """
+        long sequence of pieces in the danger frontier zone will be penalized
+        frontier zone marked with X:  . . . . . . . .
+                                      . X X X X X X .
+                                      . X . . . . X .
+                                      . X . . . . X .
+                                      . X . . . . x .
+                                      . X . . . . X .
+                                      . X X X X X X .
+                                      . . . . . . . .
+        """
         line1 = 0
         line6 = 0
         column1 = 0
@@ -81,9 +113,11 @@ class My_Heuristic(Heuristic):
 
         max_sequence1 = 0
         max_sequence6 = 0
-
+        #if outer square doesnt have much empty space, the frontier zone is not so dangerous
+        if not self.is_outer_tiles_sparse(my_board,0.3):
+            return 0
         long_seq = 2
-        for j in range(8):
+        for j in range(1,8):
             if my_board.tiles[1][j] == color:
                 line1+=1
             else:
@@ -99,7 +133,9 @@ class My_Heuristic(Heuristic):
                 line6 = 0
         line1 = max_sequence1
         line6 = max_sequence6
-        for i in range(8):
+        max_sequence1 = 0
+        max_sequence6 = 0
+        for i in range(1,8):
             if my_board.tiles[i][1] == color:
                 column1+=1
             else:
@@ -124,35 +160,36 @@ class My_Heuristic(Heuristic):
         """
     
         pieces_in_center = self.num_center_pieces(node.board,self.agent_color)
-        pieces_value = node.board.num_pieces(self.agent_color)  - node.board.num_pieces(self.opponent_color)  
+        pieces_value = node.board.num_pieces(self.agent_color)
         number_of_pieces_placed  = node.board.num_pieces(self.agent_color) + node.board.num_pieces(self.opponent_color)
         opp_legal_moves_value = len(node.board.legal_moves(self.opponent_color))
         agent_legal_moves_value = len(node.board.legal_moves(self.agent_color))
         agent_corners = self.get_num_corners(node.board,self.agent_color) 
         opp_corners = self.get_num_corners(node.board,self.opponent_color) 
-        num_corner_risk  = self.get_num_corners_at_risk(node.board,self.agent_color)
+        corner_risk  = self.get_corners_risk(node.board,self.agent_color)
 
         outer_pieces = self.get_num_of_outer_pieces(node.board,self.agent_color) 
         long_frontier_seq_penalty = self.long_frontier_sequence_penalty(node.board,self.agent_color) 
-        
+
         if node.parent == None:
-            placed_on_xsquare = 0
+            placed_on_x_tile = 0
         else:
-            placed_on_xsquare = self.x_square_occupied(node.board,self.agent_color) - self.x_square_occupied(node.parent.board,self.agent_color)
-        
-        w_xsquare = -100
-        w_corner_risk = -15
+            placed_on_x_tile = self.x_tile_occupied(node.board,self.agent_color) - self.x_tile_occupied(node.parent.board,self.agent_color)
+        w_long_frontier = 1
+        w_x_tile= -10
+        w_corner_risk = -12
         w_opp_legal_moves = -0.7 
-        w_agent_corner = 30
-        w_opp_corner = -30
-        w_pieces = 2*self.modified_sigmoid(number_of_pieces_placed)
+        w_agent_corner = 15
+        w_opp_corner = -18
+        w_pieces = self.modified_sigmoid(number_of_pieces_placed)
         w_pieces_in_center = self.inverse_modified_sigmoid(number_of_pieces_placed)
         w_outer_pieces = 0.35
         w_agent_moves = 1
         return w_pieces*pieces_value + opp_legal_moves_value*w_opp_legal_moves \
             + agent_legal_moves_value*w_agent_moves + agent_corners*w_agent_corner + opp_corners*w_opp_corner \
-            + num_corner_risk*w_corner_risk +pieces_in_center*w_pieces_in_center + outer_pieces*w_outer_pieces  + long_frontier_seq_penalty \
-            + w_xsquare*placed_on_xsquare
+            + corner_risk*w_corner_risk +pieces_in_center*w_pieces_in_center + outer_pieces*w_outer_pieces  \
+            + w_long_frontier*long_frontier_seq_penalty + w_x_tile*placed_on_x_tile
+            
     
     def modified_sigmoid(self,x):
         return 1/(1+math.e**(-((x/(4))-5)))
